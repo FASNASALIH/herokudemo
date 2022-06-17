0a3bc6e609ffea9bdf3dc4abb2697cc1d2c99f27
fasnasalih <fasnapp26@gmail.com>
2022-06-17 16:15:47 +0530

    1st commit

---
+++ resources/views/prjcthm.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> welcome...</h1>
+    <h2>sign up</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, dolore soluta at commodi, incidunt iste veritatis aut excepturi repudiandae quos est! Vel error laudantium dolorum repudiandae! Culpa ex nulla repellat.</p>
+</body>
+</html>
